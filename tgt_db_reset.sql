5648d91dbc09081d13aca11325e27115642d6097
xiaolingwei <mayiming@njust.edu.cn>
2022-12-10 17:11:43 +0800

    Create tgt_db_reset.sql

---
+++ tgt_db_reset.sql
@@ -0,0 +1,14 @@
+use calamari;
+
+delete from tgt_chap;
+delete from tgt_chap_initiator;
+delete from tgt_hg_host;
+delete from tgt_host_group;
+delete from tgt_host_group_relate_port;
+delete from tgt_initiator;
+delete from tgt_initiator_mapping;
+delete from tgt_lun_mapping;
+delete from tgt_lun_target;
+delete from tgt_node_status;
+delete from tgt_target;
+delete from tgt_host;
