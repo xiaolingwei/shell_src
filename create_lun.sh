89b490bc81652c620709b0030a577a99489d75a0
xiaolingwei <mayiming@njust.edu.cn>
2022-12-14 10:58:25 +0800

    增加 EC4+2命令、创建卷计数、创建失败提示

--- create_lun.sh
+++ create_lun.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-for i in {1..100000}
+
+# 创建多个卷脚本
+# RP3GX：onestor blk LUN_create -d "{'pool_name':'p1','lun_name':'lun-$i','lun_type':'thin','lun_size':1000,'redundancy':'replicated','replicate_num':3,'min_size':'task'}"
+# EC4+2： onestor blk LUN_create -d "{'pool_name':'pl','lun_name':'lun-$i','lun_type':'thin','lun_size':1000,'redundancy':'ec','replicate_num':{'k':4, 'm':2},'min_size':'task'}"
+
+
+for i in {1..1000}
 do
-onestor blk LUN_create -d "{'pool_name':'p1','lun_name':'ma-$i','lun_type':'thin','lun_size':100,'redundancy':'replicated','replicate_num':3,'min_size':'task'}"
+    onestor blk LUN_create -d "{'pool_name':'p1','lun_name':'lun-$i','lun_type':'thin','lun_size':1000,'redundancy':'replicated','replicate_num':3,'min_size':'task'}"
+    create_status=$?  # get return status of the last command
+
+    if (($create_status)); then
+        echo "[ERROR]The command execute failed."
+        exit 0
+    fi
+    echo "[INFO]The ${i}th lun have been created."
 done
