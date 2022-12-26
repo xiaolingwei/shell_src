27aa9dc9eee6cd028b5924fe1988382640a46bec
xiaolingwei <mayiming@njust.edu.cn>
2022-12-26 10:10:54 +0800

    适配了扩容集群的重置

--- reset.sh
+++ reset.sh
@@ -1,39 +1,58 @@
-#!/bin/bash
+#!/bin/sh
 
-handy_ip=55.149.1.83
-node_ip1=55.149.1.85
-node_ip2=55.149.1.86
+handy_ip=55.149.1.68
 
+node_ip=(55.149.1.69 55.149.1.70 55.149.1.83)
+echo "[INFO]Handy ip: $handy_ip"
+echo "[INFO]Node ip :"
+
+for var in ${node_ip[@]}
+do
+    echo $var
+done
 
 echo '[INFO]stop daos server...'
 systemctl stop daos_server.service
-ssh -q root@$node_ip1 "systemctl stop daos_server.service"
-ssh -q root@$node_ip2 "systemctl stop daos_server.service"
+for var in ${node_ip[@]}
+do
+ssh -q root@$var "systemctl stop daos_server.service"
+done
 
 echo '[INFO]umount daos0...'
 umount /mnt/daos0
-ssh -q root@$node_ip1 "umount /mnt/daos0"
-ssh -q root@$node_ip2 "umount /mnt/daos0"
+for var in ${node_ip[@]}
+do
+ssh -q root@$var "umount /mnt/daos0"
+done
 
 echo '[INFO]umount daos1...'
 umount /mnt/daos1
-ssh -q root@$node_ip1 "umount /mnt/daos1"
-ssh -q root@$node_ip2 "umount /mnt/daos1"
+for var in ${node_ip[@]}
+do
+ssh -q root@$var "umount /mnt/daos1"
+done
+
 
 echo '[INFO]format pmem0...'
 wipefs -a /dev/pmem0
-ssh -q root@$node_ip1 "wipefs -a /dev/pmem0"
-ssh -q root@$node_ip2 "wipefs -a /dev/pmem0"
+for var in ${node_ip[@]}
+do
+ssh -q root@$var "wipefs -a /dev/pmem0"
+done
 
 echo '[INFO]format pmem1...'
 wipefs -a /dev/pmem1
-ssh -q root@$node_ip1 "wipefs -a /dev/pmem1"
-ssh -q root@$node_ip2 "wipefs -a /dev/pmem1"
+for var in ${node_ip[@]}
+do
+ssh -q root@$var "wipefs -a /dev/pmem1"
+done
 
 echo '[INFO]start daos server...'
 systemctl start daos_server.service
-ssh -q root@$node_ip1 "systemctl start daos_server.service"
-ssh -q root@$node_ip2 "systemctl start daos_server.service"
+for var in ${node_ip[@]}
+do
+ssh -q root@$var "systemctl start daos_server.service"
+done
 
 #echo 'prepare scm ...'
 #/opt/h3c/bin/daos_server storage prepare --scm-only
@@ -57,8 +76,10 @@ ssh -q root@$node_ip2 "systemctl start daos_server.service"
 
 echo '[INFO]restart daos agent...'
 systemctl restart daos_agent.service
-ssh -q root@$node_ip1 "systemctl restart daos_agent.service"
-ssh -q root@$node_ip2 "systemctl restart daos_agent.service"
+for var in ${node_ip[@]}
+do
+ssh -q root@$var "systemctl restart daos_agent.service"
+done
 
 sleep 10
 /opt/h3c/bin/dmg storage format
@@ -76,7 +97,11 @@ sleep 2
 
 echo '[INFO]restart tgt...'
 service tgt restart
-ssh -q root@$node_ip1 "service tgt restart"
-ssh -q root@$node_ip2 "service tgt restart"
+for var in ${node_ip[@]}
+do
+ssh -q root@$var "service tgt restart"
+done
 
 echo '[INFO]OK! Now you can operate the cluster'
+
+
