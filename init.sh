2828c26ab97fe25192588533081b3b4c15c6c8d9
xiaolingwei <mayiming@njust.edu.cn>
2023-01-31 16:15:20 +0800

    增加vim 设置

--- init.sh
+++ init.sh
@@ -21,3 +21,46 @@ EOF
 
 echo "sh /etc/init.d/cx.sh" >> /etc/rc.local
 chmod 755 /etc/rc.local
+
+
+touch ~/.vimrc
+cat > /etc/init.d/cx.sh <<- EOF
+
+"语法高亮
+syntax enable
+syntax on
+"显示行号
+set nu
+"显示括号匹配
+set showmatch
+"括号匹配显示时间为1(单位是十分之一秒)
+set matchtime=5
+"显示当前的行号列号：
+set ruler
+"在状态栏显示正在输入的命令
+set showcmd
+" 显示Tab符，使用一高亮竖线代替
+set list
+set listchars=tab:\|\ ,
+set listchars=tab:>-,trail:-
+"设置高亮搜索
+set hlsearch
+"在搜索时，输入的词句的逐字符高亮
+set incsearch
+line("$") |
+    \   exe "normal g`\"" |
+    \ endif
+
+  augroup END
+
+else
+
+  set autoindent                " always set autoindenting on
+
+endif " has("autocmd")
+
+" 设置tab是四个空格
+set ts=4
+set expandtab
+
+EOF
