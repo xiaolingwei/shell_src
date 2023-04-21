be3c2e374e3cf2e4f4dfa35d9fd4e87b55736531
xiaolingwei <mayiming@njust.edu.cn>
2023-04-21 11:29:49 +0800

    Update log_config.sh

--- log_config.sh
+++ log_config.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 # 日志文件daos_server.yml配置脚本
-# 脚本功能：配置集群所有节点daos_server.yml日志
-# 使用方法：在集群Handy节点、任一位置执行脚本即可配置集群所有节点daos_server.yml日志，脚本会自动获取集群节点IP.
-daos_server_path="etc/daos/daos_server.yml" # 需要同步修改revise函数中路径
+# 脚本功能：配置集群所有节点daos_server.yml,daos_block.yml日志
+# 使用方法：修改开头集群列表，在集群Handy节点、任一位置执行脚本即可配置集群所有节点daos_server.yml,daos_block.yml
 
+nodes=(55.149.1.83 55.149.1.85 55.149.1.86) # 节点列表
+
+daos_server_path="/etc/daos/daos_server.yml" # 需要同步修改revise函数中路径
 # content to revise
 function revise {
+        # 设置开启DEBUG日志的模块 - DD_SUBSYS前空两个空格
         sed -i '/- FI_OFI_RXM_USE_SRX=1/i \
-  - DAOS_MD_CAP=1024\
-  - CRT_TIMEOUT=120\
-  - D_LOG_SIZE=5g\
-  - D_LOG_MASK=DEBUG\
-  - DD_SUBSYS=all\
+  - DD_SUBSYS= - DD_SUBSYS=vos,tree,dtx,pool,container,rebuild,server,array,rpc,object\
   - DD_MASK=all\
-  - HG_LOG_SUBSYS=hg,na,cls,op\
-  - HG_LOG_LEVEL=debug\
-  - HG_NA_LOG_LEVEL=debug' /etc/daos/daos_server.yml ;
+  - SWIM_ROCEV_TIMEOUT=60' /etc/daos/daos_server.yml ;
         sed -i "s/INFO/DEBUG/g" /etc/daos/daos_server.yml ;
+        # 设置客户端DEBUG
+        sed -i "s/INFO/DEBUG/g" /etc/daos/daos_block.yml ;
 }
 
 # interact (y/n)
@@ -32,28 +31,9 @@ function yes_or_no {
         fi
 }
 
-# check current node id
-function check_node {
-    # $1 nodes $2 num_nodes
-        array="`ifconfig -a |grep -v inet6 |grep inet|grep -v 127.0.0.1|awk '{print $2}'`"
-        nodes=$1
-        num_nodes=$2
-        for var in ${array[@]}
-        do
-                for((i=0; i<$num_nodes; i++))
-                do
-
-                if [[ "$var" == $nodes[$i] ]]; then
-                        return $i
-                fi
-                done
-        done
-
-}
-
 # check if already configrution
 function already_config {
-        result=$(grep D_LOG_MASK $daos_server_path)
+        result=$(grep DD_SUBSYS $daos_server_path)
         if [[ $result != "" ]]; then
                 echo "[ERROR]The config file already configured "
                 return 1
@@ -61,11 +41,6 @@ function already_config {
                 return 0
         fi
 }
-
-
-
-
-
 
 # if the config file exist
 if [ -e $daos_server_path ]; then
@@ -77,47 +52,19 @@ if [ -e $daos_server_path ]; then
                 exit 0
         fi
 
-
-        # get the ip of nodes
-        num_nodes=`sed -n '/[0-9]\{1,\}.[0-9]\+.[0-9]\+.[0-9]\+$/p' $daos_server_path | awk '{print $2}'| wc -l`
-        echo "[INFO]The nodes number of the cluster is $num_nodes"
-        nodes[$((num_nodes-1))]=""
-        for ((i=1; i<=$num_nodes; i++));
-        do
-            nodes[(($i-1))]=`sed -n '/[0-9]\{1,3\}.[0-9]\+.[0-9]\+.[0-9]\+$/p' $daos_server_path | awk '{print $2}'| sed -n "${i}p"`
-
-        done
-
-        echo "[INFO]The nodes IP in the cluster are ${nodes[@]}"
-
-        # check current node ip
-        check_node $nodes $num_nodes
-        node_id=$?
-        echo "[INFO]Current node IP is ${nodes[$node_id]}"
-
         # revise remote node config file
-        for ((i=0; i<$num_nodes; i++))
+        for node in ${nodes[@]}
         do
-                if [[ $i -ne $node_id ]]; then
-                        ssh ${nodes[$i]}  "$(typeset -f revise); revise"
-                fi
+            ssh $node  "$(typeset -f revise); revise"
         done
-
-        revise # revise current node config file
-
 
         echo "[INFO] The log configuration success"
 
-
         # restart daos_server
-
-        for ((i=0; i<$num_nodes; i++))
+        for node in ${nodes[@]}
         do
-                if [[ $i -ne $node_id ]]; then
-                        ssh ${nodes[$i]} "systemctl restart daos_server.service"
-                fi
+            ssh $node "systemctl restart daos_server.service"
         done
-        systemctl restart daos_server.service
         echo "[INFO] daos_server.service restart success"
 
 else
